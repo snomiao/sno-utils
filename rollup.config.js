e2a752b5163388d61d6f250da1acfb605d2c81ec
snomiao <snomiao@gmail.com>
2021-11-22 14:32:32 +0800

    fix: complete test

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,4 @@
-// // rollup.config.js
-// import typescript from '@rollup/plugin-typescript';
-
-// export default {
-//   input: 'src/index.ts',
-//   output: {
-//     dir: 'output',
-//     format: 'cjs'
-//   },
-//   dir: '',
-//   plugins: [typescript()]
-// };
-
-// import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
-
 export default {
     input: "lib/index.js",
     output: [
@@ -27,33 +12,5 @@ export default {
         },
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-
-    plugins: [
-        // typescript({
-        //   // typescript: require('typescript'),
-        // }),
-    ],
+    plugins: [],
 };
-// export default {
-//   input: 'src/index.ts',
-//   output: [
-//     {
-//       file: pkg.main,
-//       format: 'cjs',
-//     },
-//     {
-//       file: pkg.module,
-//       format: 'es',
-//     },
-//   ],
-//   external: [
-//     ...Object.keys(pkg.dependencies || {}),
-//     ...Object.keys(pkg.peerDependencies || {}),
-//   ],
-
-//   plugins: [
-//     typescript({
-//       typescript: require('typescript'),
-//     }),
-//   ],
-// }
