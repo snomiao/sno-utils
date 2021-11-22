02d49f22bfc29e4b609b6a2638e064d4beb44db6
snomiao <snomiao@gmail.com>
2021-11-22 19:32:46 +0800

    fix: types

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,18 @@ export default {
         {
             file: pkg.main,
             format: "cjs",
+            sourcemap: true,
         },
         {
             file: pkg.module,
-            format: "es",
+            format: "esm",
+            sourcemap: true,
+        },
+        {
+            file: pkg.umd,
+            name: "snoUtils",
+            format: "umd",
+            sourcemap: true,
         },
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
