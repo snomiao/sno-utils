96aca129d6e4e7061a1ca8162d3b5e4c935ac5f3
snomiao <snomiao@gmail.com>
2021-11-23 17:57:33 +0800

    feat: 文本文件缓存

--- src/文件.ts
+++ src/文件.ts
@@ -13,3 +13,9 @@ export const 文本文件读取 = async (路径: string) => await (await fsp()).
  * NODE-ONLY
  */
 export const 文件写入 = async (路径: string, 数据: string | Uint8Array) => await (await fsp()).writeFile(路径, 数据);
+export const 文本文件缓存 = async (路径: string, 内容函数: () => Promise<any>) =>
+    await 文本文件读取(路径).catch(async () => {
+        const 内容 = await 内容函数();
+        await 文件写入(路径, 内容);
+        return 内容;
+    });
